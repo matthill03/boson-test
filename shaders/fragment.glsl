b0309b7937fb6ee46cb348893802d25ed894c6bb
Matthew Hill <contact@matthew-hill.net>
2025-01-04 23:39:40 +0000

    adding 3d

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,7 +1,38 @@
 #version 410 core
 out vec4 frag_colour;
 
+in vec3 norm;
+in vec3 frag_pos;
+
+struct Material {
+    vec3 colour;
+};
+
+uniform Material material;
+uniform vec3 light_pos;
+uniform vec3 view_pos;
+
 void main()
 {
-    frag_colour = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    // ambient
+    float ambient_strength = 0.2;
+    vec3 ambient = ambient_strength * vec3(1.0f, 1.0f, 1.0f);
+
+    // diffuse 
+    vec3 normal = normalize(norm);
+    vec3 light_dir = normalize(light_pos - frag_pos);
+    float diff = max(dot(normal, light_dir), 0.0);
+    vec3 diffuse = diff * vec3(1.0f, 1.0f, 1.0f);
+
+    // specular
+    float specular_strength = 0.5;
+    vec3 view_dir = normalize(view_pos - frag_pos);
+    vec3 reflect_dir = reflect(-light_dir, normal);
+    float spec = pow(max(dot(view_dir, reflect_dir), 0.0), 128);
+    vec3 specular = specular_strength * spec * vec3(1.0f, 1.0f, 1.0f);
+
+    vec3 result = (ambient * diffuse * specular) * material.colour;
+
+    frag_colour = vec4(result, 1.0f);
 }
+
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,9 +1,20 @@
 #version 410 core
 layout (location = 0) in vec3 position;
+layout (location = 1) in vec3 normal;
+layout (location = 2) in vec3 tex_coord;
 
-uniform mat4 transform;
+out vec3 norm;
+out vec3 frag_pos;
+
+uniform mat4 view;
+uniform mat4 projection;
+uniform mat4 model;
 
 void main()
 {
-    gl_Position = transform * vec4(position, 1.0);
+    frag_pos = vec3(model * vec4(position, 1.0));
+    norm = mat3(transpose(inverse(model))) * normal;
+
+    gl_Position = projection * view * model * vec4(position, 1.0);
 }
+
