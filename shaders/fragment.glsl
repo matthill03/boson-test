72847768ff795ae54118120010de10f2e4c88b5a
Matthew Hill <contact@matthew-hill.net>
2025-01-05 14:35:23 +0000

    Add some simple lighting
    Also increase window size

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -5,7 +5,10 @@ in vec3 norm;
 in vec3 frag_pos;
 
 struct Material {
-    vec3 colour;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
 };
 
 uniform Material material;
@@ -15,23 +18,22 @@ uniform vec3 view_pos;
 void main()
 {
     // ambient
-    float ambient_strength = 0.2;
-    vec3 ambient = ambient_strength * vec3(1.0f, 1.0f, 1.0f);
+    float ambient_strength = 0.1;
+    vec3 ambient = material.ambient * vec3(1.0f, 1.0f, 1.0f);
 
-    // diffuse 
+    // diffuse
     vec3 normal = normalize(norm);
     vec3 light_dir = normalize(light_pos - frag_pos);
     float diff = max(dot(normal, light_dir), 0.0);
-    vec3 diffuse = diff * vec3(1.0f, 1.0f, 1.0f);
+    vec3 diffuse = (diff * material.diffuse) * vec3(1.0f, 1.0f, 1.0f);
 
     // specular
-    float specular_strength = 0.5;
     vec3 view_dir = normalize(view_pos - frag_pos);
     vec3 reflect_dir = reflect(-light_dir, normal);
-    float spec = pow(max(dot(view_dir, reflect_dir), 0.0), 128);
-    vec3 specular = specular_strength * spec * vec3(1.0f, 1.0f, 1.0f);
+    float spec = pow(max(dot(view_dir, reflect_dir), 0.0), material.shininess);
+    vec3 specular = (material.specular * spec) * vec3(1.0f, 1.0f, 1.0f);
 
-    vec3 result = (ambient * diffuse * specular) * material.colour;
+    vec3 result = (ambient + diffuse + specular);
 
     frag_colour = vec4(result, 1.0f);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "boson/application.h"
 
 int main() {
-    boson::Application app({1080, 720, "Hello World"});
+    boson::Application app({1280, 720, "Hello World"});
     app.run();
     return 0;
 }
