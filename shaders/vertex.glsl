3c3e1b3b04a07df4e2985f88cd180337e1c3e502
Matthew Hill <contact@matthew-hill.net>
2025-01-06 16:07:32 +0000

    in progress instance rendering

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -3,13 +3,16 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
 layout (location = 2) in vec2 tex_coord;
 
+// Per instance info
+layout (location = 3) in mat4 model;
+
 out vec3 norm;
 out vec3 frag_pos;
 out vec2 texture_coord;
 
 uniform mat4 view;
 uniform mat4 projection;
-uniform mat4 model;
+//uniform mat4 model;
 
 void main()
 {
