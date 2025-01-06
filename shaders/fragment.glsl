c89f7f54d7b80ec6ddcfa21ebd4483e4af76e2a8
Matthew Hill <contact@matthew-hill.net>
2025-01-06 00:00:17 +0000

    Add resources for changes

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -3,11 +3,11 @@ out vec4 frag_colour;
 
 in vec3 norm;
 in vec3 frag_pos;
+in vec2 texture_coord;
 
 struct Material {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+    sampler2D diffuse;
+    sampler2D specular;
     float shininess;
 };
 
@@ -19,19 +19,19 @@ void main()
 {
     // ambient
     float ambient_strength = 0.1;
-    vec3 ambient = material.ambient * vec3(1.0f, 1.0f, 1.0f);
+    vec3 ambient = vec3(texture(material.diffuse, texture_coord)) * vec3(1.0f, 1.0f, 1.0f);
 
     // diffuse
     vec3 normal = normalize(norm);
     vec3 light_dir = normalize(light_pos - frag_pos);
     float diff = max(dot(normal, light_dir), 0.0);
-    vec3 diffuse = (diff * material.diffuse) * vec3(1.0f, 1.0f, 1.0f);
+    vec3 diffuse = (diff * vec3(texture(material.diffuse, texture_coord))) * vec3(1.0f, 1.0f, 1.0f);
 
     // specular
     vec3 view_dir = normalize(view_pos - frag_pos);
     vec3 reflect_dir = reflect(-light_dir, normal);
     float spec = pow(max(dot(view_dir, reflect_dir), 0.0), material.shininess);
-    vec3 specular = (material.specular * spec) * vec3(1.0f, 1.0f, 1.0f);
+    vec3 specular = (vec3(texture(material.specular, texture_coord)) * spec) * vec3(1.0f, 1.0f, 1.0f);
 
     vec3 result = (ambient + diffuse + specular);
 
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,10 +1,11 @@
 #version 410 core
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
-layout (location = 2) in vec3 tex_coord;
+layout (location = 2) in vec2 tex_coord;
 
 out vec3 norm;
 out vec3 frag_pos;
+out vec2 texture_coord;
 
 uniform mat4 view;
 uniform mat4 projection;
@@ -12,6 +13,7 @@ uniform mat4 model;
 
 void main()
 {
+    texture_coord = tex_coord;
     frag_pos = vec3(model * vec4(position, 1.0));
     norm = mat3(transpose(inverse(model))) * normal;
 
